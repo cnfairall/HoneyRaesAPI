6839afb0b7de83714e64b2554422f23fdeeae522
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-02-03 14:30:49 -0600

    added id

--- Program.cs
+++ Program.cs
@@ -104,11 +104,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/api/servicetickets", () =>
 {
     return serviceTickets;
